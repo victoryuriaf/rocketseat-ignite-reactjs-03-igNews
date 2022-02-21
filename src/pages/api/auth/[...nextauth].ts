4ffd8c7dc526940acabbdf6f375ca8de9aa6c1df
Victor Yuri <victoryuri.af@gmail.com>
2022-02-21 10:39:04 -0300

    using faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query } from 'faunadb';
+
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
     providers: [
@@ -13,4 +17,39 @@ export default NextAuth({
             }
         }),
     ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+            const { email } = user
+
+            try {
+                await fauna.query(
+                    query.If(
+                        query.Not(
+                            query.Exists(
+                                query.Match(
+                                    query.Index('user_by_email'),
+                                    query.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        query.Create(
+                            query.Collection('users'),
+                            { data: { email } }
+                        ),
+                        query.Get(
+                            query.Match(
+                                query.Index('user_by_email'),
+                                query.Casefold(user.email)
+                            )
+                        )
+                    )
+                )
+
+                return true
+
+            } catch {
+                return false
+            }
+        },
+    }
 })
